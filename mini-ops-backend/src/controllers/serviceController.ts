a46055e3ddc20cefec88c0e5fc87781d949169e0
Mudassar Majeed <mudasarmajeed5@yahoo.com>
2026-06-27 17:13:38 +0500

    added serviceController.ts

--- mini-ops-backend/src/controllers/serviceController.ts
+++ mini-ops-backend/src/controllers/serviceController.ts
@@ -1,12 +1,12 @@
-import type {Request, Response, NextFunction} from "express";
-import {services} from "../db/schema.ts";
+import type {Request, Response} from "express";
+import {Service, services} from "../db/schema.ts";
 import db from "../db/connection.ts";
-import {eq} from "drizzle-orm/pg-core"
+import {eq} from "drizzle-orm"
 
-export const addService = async (req: Request, res: Response) => {
+export const createService = async (req: Request, res: Response) => {
     try {
         const {baseUrl} = req.body;
-        const [existingService] = await db.select().from(services).where(eq(service.baseUrl, baseUrl))
+        const [existingService] = await db.select().from(services).where(eq(services.baseUrl, baseUrl))
         if (existingService) {
             return res.status(409).json({message: "Service already exists", success: false})
         }
@@ -26,13 +26,37 @@ export const addService = async (req: Request, res: Response) => {
 
 export const deleteService = async (req: Request, res: Response) => {
     try {
-        const {deleteId} = req.params.id
-        const [deletedService] = await db.delete(services).where(eq(service.id, deleteId)).returning()
+        const deleteId = req.params.id as string;
+        const [deletedService] = await db.delete(services).where(eq(services.id, deleteId)).returning()
         if (!deletedService) {
             return res.status(404).json({message: "Service not found", success: false})
         }
         return res.status(200).json({message: "Service deleted", success: true, deletedService})
     } catch (error) {
+        return res.status(500).json({message: (error as Error).message, success: false})
+    }
+}
+export const updateService = async (req: Request, res: Response) => {
+    try {
+        const updateId = req.params.id as string;
+        const {baseUrl} = req.body;
+        const [updateService] = await db.update(services).set({
+            baseUrl
+        }).where(eq(services.id, updateId)).returning()
+        if (!updateService) {
+            return res.status(404).json({message: "Service not found", success: false});
+        }
+        return res.status(200).json({message: "Service updated", success: true, updateService})
+    } catch {
+
+    }
+}
+export const getServices = async (req: Request, res: Response) => {
+    try {
+        const userId = req.user!.id;
+        const userServices:Service[] = await db.select().from(services).where(eq(services.userId, userId));
+        return res.status(200).json({services: userServices || []})
+    } catch(error) {
         return res.status(500).json({message: (error as Error).message, success: false})
     }
 }
